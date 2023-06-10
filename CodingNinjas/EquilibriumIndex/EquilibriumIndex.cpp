6de80ccc29a11947c8720eaa4f65b8c9e49d1c24
Rahul Deb Bhandari <Rahul@Deb.Bhandari>
2023-06-10 10:36:18 +0530

    git ignore is updated

---
+++ CodingNinjas/EquilibriumIndex/EquilibriumIndex.cpp
@@ -0,0 +1,37 @@
+// https://www.youtube.com/watch?v=iL7oSNc3OXA
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool isPossible(int *arr, int n){
+    bool changed = false;
+
+    int pos = -1;
+    for (int i = 0; i < (n-1); i++){
+        if(arr[i] > arr[i+1]){
+            if(pos != -1){
+                return false;
+            }
+            pos = i;
+        }
+    }
+
+    return pos == (-1) || pos == 0 || pos == (n-2) || arr[pos -1] <=  arr[pos +1] || arr[pos] <=  arr[pos +2];
+}
+
+int main(){
+    int t, n, k, temp;
+    vector <int> arr;
+
+    cin >> t;
+
+    // for each testcase
+    for(int i = 0; i < t; i++){
+        cin >> n;
+        int arr [n];
+        for(int j = 0; j < n; j++){
+            cin >> arr[j]; 
+        }
+        cout << boolalpha << isPossible(arr, n) << endl;
+    }
+}
